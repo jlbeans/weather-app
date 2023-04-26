ba4e6d6a5b2419484d112321690812cefe096b25
jlbeans <jm@cselmer.com>
2023-04-26 10:48:14 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const name = 'James';
+
+const person = { first: name };
+
+console.log(person);
+
+// eslint-disable-next-line no-unused-vars
+const sayHelloLinting = (fName) => {
+	console.log(`Hello linting, ${fName}`);
+};
